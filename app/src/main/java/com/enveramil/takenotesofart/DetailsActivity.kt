182733fe8b954b7813b6301bbb2870074eccea53
Enver Amil <enveramil@hotmail.com>
2022-04-29 09:21:37 +0300

    Revert "Added permission process"
    This reverts commit 3449c77030afa5da80e3418b12efd2c148d7d854.

--- app/src/main/java/com/enveramil/takenotesofart/DetailsActivity.kt
+++ app/src/main/java/com/enveramil/takenotesofart/DetailsActivity.kt
@@ -1,16 +1,9 @@
 package com.enveramil.takenotesofart
 
-import android.content.Intent
-import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.View
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.enveramil.takenotesofart.databinding.ActivityDetailsBinding
-import com.google.android.material.snackbar.Snackbar
-import java.util.jar.Manifest
 
 class DetailsActivity : AppCompatActivity() {
     private lateinit var binding : ActivityDetailsBinding
@@ -20,21 +13,7 @@ class DetailsActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
     }
-    fun selectImage(view: View){
-        if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
-            // izin verilmediğindeki durumu kontrol ediyoruz.
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)){
-                Snackbar.make(view,"Permission needed",Snackbar.LENGTH_INDEFINITE).setAction("Give permission",View.OnClickListener {
-                    // izin istenilecek
-                })
-            }else{
-                // izin istenecek
-            }
-        }else{
-            // izin verildiği zamanki durumu kontrol ediyoruz.
-            val intentToGallery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        }
-    }
+    fun selectImage(view: View){}
 
     fun save(view: View){}
 }
